7b82378580ceca3cfd1f013c252fffd7f0c36662
MBJean <michaelbrianjean@gmail.com>
2021-01-16 09:41:16 -0500

    Init Gatsby with sensible defaults

---
+++ gatsby-config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  siteMetadata: {
+    title: "gatsby-js-demo-et",
+  },
+  plugins: [
+    "gatsby-plugin-sass",
+    "gatsby-plugin-sharp",
+    "gatsby-plugin-react-helmet",
+    "gatsby-plugin-sitemap",
+    "gatsby-transformer-sharp",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "images",
+        path: "./src/images/",
+      },
+      __key: "images",
+    },
+  ],
+};
